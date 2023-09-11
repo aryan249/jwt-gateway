96c3f0146818118f454bfe7c361c0d960e43e595
aryan249 <aryan.soni249@gmail.com>
2023-09-11 11:11:01 +0530

    Add role extraction from JWT claims

--- src/middleware/rbac.ts
+++ src/middleware/rbac.ts
@@ -1,7 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
+import { ForbiddenError, AuthenticationError } from '../utils/errors';
 
-export const rbac = (requiredRoles: string[]) => {
-  return (req: Request, _res: Response, next: NextFunction) => {
+export function requireRoles(allowedRoles: string[]) {
+  return (req: Request, _res: Response, next: NextFunction): void => {
+    if (!req.user) {
+      next(new AuthenticationError('User not authenticated'));
+      return;
+    }
+
+    const userRoles = req.user.roles || [];
+    const hasRole = allowedRoles.some((role) => userRoles.includes(role));
+
+    if (!hasRole) {
+      next(
+        new ForbiddenError(
+          `Required roles: [${allowedRoles.join(', ')}]. User roles: [${userRoles.join(', ')}]`,
+        ),
+      );
+      return;
+    }
+
     next();
   };
-};
+}
