32f245c0c2377e10fd364e3807cb29e7fe004953
aryan249 <aryan.soni249@gmail.com>
2023-09-19 02:39:23 +0530

    Implement request header forwarding to upstream

--- src/routes/gateway.ts
+++ src/routes/gateway.ts
@@ -1,5 +1,36 @@
 import { Router } from 'express';
+import { jwtAuth, requireRoles, rateLimiter } from '../middleware';
+import { createServiceProxy } from '../services/proxy';
+import { logger } from '../utils/logger';
+import type { GatewayConfig } from '../types';
 
-const router = Router();
+export function createGatewayRouter(config: GatewayConfig): Router {
+  const router = Router();
 
-export default router;
+  for (const service of config.services) {
+    logger.info(`Registering route: ${service.prefix} -> ${service.upstream}`, {
+      service: service.name,
+      roles: service.roles,
+      rateLimit: `${service.rateLimit.max} req / ${service.rateLimit.windowMs}ms`,
+    });
+
+    const middlewares = [
+      rateLimiter({
+        windowMs: service.rateLimit.windowMs,
+        max: service.rateLimit.max,
+        failOpen: config.rateLimitFailOpen,
+      }),
+      jwtAuth(config.jwks.issuer, config.jwks.audience),
+      requireRoles(service.roles),
+    ];
+
+    const proxy = createServiceProxy(
+      service.upstream,
+      service.stripPrefix ? service.prefix : undefined,
+    );
+
+    router.use(service.prefix, ...middlewares, proxy);
+  }
+
+  return router;
+}
