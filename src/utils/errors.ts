9d0b195abadbe7c44eafc3a56965a35bd4defc9e
aryan249 <aryan.soni249@gmail.com>
2023-10-10 22:12:30 +0530

    Add ForbiddenError and RateLimitError classes

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,10 +1,35 @@
 export class AppError extends Error {
-  constructor(public statusCode: number, message: string) {
+  constructor(
+    message: string,
+    public statusCode: number,
+    public code: string,
+  ) {
     super(message);
-    this.name = 'AppError';
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
-export class NotFoundError extends AppError {
-  constructor(message = 'Not Found') { super(404, message); }
+export class AuthenticationError extends AppError {
+  constructor(message = 'Authentication required') {
+    super(message, 401, 'AUTHENTICATION_ERROR');
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message = 'Insufficient permissions') {
+    super(message, 403, 'FORBIDDEN');
+  }
+}
+
+export class RateLimitError extends AppError {
+  constructor(message = 'Rate limit exceeded') {
+    super(message, 429, 'RATE_LIMIT_EXCEEDED');
+  }
+}
+
+export class ServiceUnavailableError extends AppError {
+  constructor(message = 'Service unavailable') {
+    super(message, 503, 'SERVICE_UNAVAILABLE');
+  }
 }
