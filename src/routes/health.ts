9164cc20aaa7a5f04c561664b678dbbe891976c5
aryan249 <aryan.soni249@gmail.com>
2023-11-08 10:39:03 +0530

    Add health check response with uptime and version info

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,9 +1,31 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+import { isRedisHealthy } from '../services/redis';
+import { getJwksClient } from '../services/jwks';
 
 const router = Router();
 
-router.get('/health', (_req: Request, res: Response) => {
-  res.json({ status: 'ok' });
+router.get('/health', (_req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
+});
+
+router.get('/ready', async (_req, res) => {
+  const checks: Record<string, boolean> = {};
+
+  checks.redis = await isRedisHealthy();
+  checks.jwks = getJwksClient() !== null;
+
+  const allHealthy = Object.values(checks).every(Boolean);
+
+  res.status(allHealthy ? 200 : 503).json({
+    status: allHealthy ? 'ready' : 'not ready',
+    checks,
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
 });
 
 export default router;
