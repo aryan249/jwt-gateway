fc4a4a4458cfd3a5eb781de5e79521b20826d50b
aryan249 <aryan.soni249@gmail.com>
2023-11-08 21:49:49 +0530

    Add Winston logger with structured JSON output

---
+++ src/utils/logger.ts
@@ -0,0 +1,25 @@
+import winston from 'winston';
+
+const { combine, timestamp, json, colorize, printf } = winston.format;
+
+const devFormat = combine(
+  colorize(),
+  timestamp({ format: 'HH:mm:ss' }),
+  printf(({ timestamp, level, message, ...meta }) => {
+    const metaStr = Object.keys(meta).length ? ` ${JSON.stringify(meta)}` : '';
+    return `${timestamp} ${level}: ${message}${metaStr}`;
+  }),
+);
+
+const prodFormat = combine(timestamp(), json());
+
+export function createLogger(level = 'info') {
+  return winston.createLogger({
+    level,
+    format: process.env.NODE_ENV === 'production' ? prodFormat : devFormat,
+    transports: [new winston.transports.Console()],
+    silent: process.env.NODE_ENV === 'test',
+  });
+}
+
+export const logger = createLogger(process.env.LOG_LEVEL || 'info');
