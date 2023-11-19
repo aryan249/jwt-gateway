f1804f06cf47ce8969507f74d6c6e17c0bc3583e
aryan249 <aryan.soni249@gmail.com>
2023-11-19 14:47:27 +0530

    Add Redis connection retry logic with backoff

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,10 +1,43 @@
 import Redis from 'ioredis';
+import { logger } from '../utils/logger';
 
-let client: Redis | null = null;
+let redisClient: Redis | null = null;
 
-export const getRedisClient = (): Redis => {
-  if (!client) {
-    client = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
-  }
+export function createRedisClient(url: string): Redis {
+  const client = new Redis(url, {
+    maxRetriesPerRequest: 3,
+    retryStrategy(times) {
+      if (times > 5) return null;
+      return Math.min(times * 200, 2000);
+    },
+    lazyConnect: true,
+  });
+
+  client.on('connect', () => logger.info('Redis connected'));
+  client.on('reconnecting', () => logger.warn('Redis reconnecting'));
+  client.on('error', (err) => logger.error('Redis error', { error: err.message }));
+
+  redisClient = client;
   return client;
-};
+}
+
+export function getRedisClient(): Redis | null {
+  return redisClient;
+}
+
+export async function isRedisHealthy(): Promise<boolean> {
+  if (!redisClient) return false;
+  try {
+    const result = await redisClient.ping();
+    return result === 'PONG';
+  } catch {
+    return false;
+  }
+}
+
+export async function disconnectRedis(): Promise<void> {
+  if (redisClient) {
+    await redisClient.quit();
+    redisClient = null;
+  }
+}
