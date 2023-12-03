117e2f4c5c5ae1e2721b39de8d0404f95532e8f0
aryan249 <aryan.soni249@gmail.com>
2023-12-03 17:52:45 +0530

    Write unit tests for JWKS caching service

--- test/unit/services/jwks.test.ts
+++ test/unit/services/jwks.test.ts
@@ -1,5 +1,29 @@
-describe('JWKS Service', () => {
-  it('should fetch signing keys', () => {
-    expect(true).toBe(true);
+import * as jwksService from '../../../src/services/jwks';
+import * as redisService from '../../../src/services/redis';
+import { AuthenticationError } from '../../../src/utils/errors';
+
+jest.mock('../../../src/services/redis');
+
+describe('jwks service', () => {
+  const mockGetRedisClient = redisService.getRedisClient as jest.MockedFunction<
+    typeof redisService.getRedisClient
+  >;
+
+  beforeEach(() => {
+    mockGetRedisClient.mockReturnValue(null);
+  });
+
+  it('throws when JWKS client is not initialized', async () => {
+    await expect(
+      jwksService.verifyToken('some-token', 'issuer', 'audience'),
+    ).rejects.toThrow(AuthenticationError);
+  });
+
+  it('throws on invalid token format', async () => {
+    jwksService.initJwksClient('https://example.com/.well-known/jwks.json');
+
+    await expect(
+      jwksService.verifyToken('not-a-jwt', 'issuer', 'audience'),
+    ).rejects.toThrow(AuthenticationError);
   });
 });
