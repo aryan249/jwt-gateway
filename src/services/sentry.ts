664234d07c949cf7ea8ba5d14d6a08301a5c470d
aryan249 <aryan.soni249@gmail.com>
2023-12-24 13:32:42 +0530

    Configure Sentry DSN from environment variables

--- src/services/sentry.ts
+++ src/services/sentry.ts
@@ -1,7 +1,33 @@
 import * as Sentry from '@sentry/node';
+import { logger } from '../utils/logger';
 
-export const initSentry = () => {
-  if (process.env.SENTRY_DSN) {
-    Sentry.init({ dsn: process.env.SENTRY_DSN });
+let initialized = false;
+
+export function initSentry(dsn: string): void {
+  if (!dsn || initialized) return;
+
+  Sentry.init({
+    dsn,
+    environment: process.env.NODE_ENV || 'development',
+    tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.2 : 1.0,
+    serverName: process.env.HOSTNAME || 'jwt-gateway',
+  });
+
+  initialized = true;
+  logger.info('Sentry initialized');
+}
+
+export function captureError(error: Error, context?: Record<string, unknown>): void {
+  if (context) {
+    Sentry.withScope((scope) => {
+      Object.entries(context).forEach(([key, value]) => {
+        scope.setExtra(key, value);
+      });
+      Sentry.captureException(error);
+    });
+  } else {
+    Sentry.captureException(error);
   }
-};
+}
+
+export { Sentry };
