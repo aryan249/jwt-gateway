c6491c541158a7d3d268346df3875d4cb3be3ec7
aryan249 <aryan.soni249@gmail.com>
2024-02-01 03:12:09 +0530

    Configure Jest with TypeScript transformer

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/index.ts',
+    '!src/types/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  clearMocks: true,
+  restoreMocks: true,
+  forceExit: true,
+};
+
+export default config;
